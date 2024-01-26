ef92835b0edbf8b647074432d67ab2321649244e
old-mamont <ehot771@gmail.com>
2024-01-27 00:22:54 +0500

    Подключение к базе и Запросы к базе (запись/чтение)

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import telebot
 from telebot import types
 from Token import token
-from models import verification_user
+from models import Base_SQL
+
+def verification_user(user : dict):
+    """Проверка на нового пользователя"""
+    ls_id_users = Base_SQL.show("SELECT id FROM User")
+    if not (user.id,) in ls_id_users:
+        request = """INSERT INTO User (id, first_name, last_name) VALUES ( %s, %s, %s )"""
+        data = [(user.id, user.first_name, str(user.last_name))]
+        Base_SQL.write(request, data)
+        print(f"Произошла запись пользователя: {user.id}, погоняло {user.first_name}")
+    else:
+        print(f"Пользователь {user.first_name} уже в базе")
+        
+
 
 # Создание объекта бота
 bot = telebot.TeleBot(token())
@@ -10,7 +23,8 @@ bot = telebot.TeleBot(token())
 @bot.message_handler(commands=["start"]) 
 def start(message):
     bot.send_message(message.from_user.id, "Команды:\n/start\n/button")
-    print(verification_user(message.from_user))
+    verification_user(message.from_user)
+    # print((message.from_user))
 
 # Обработчик команды /button
 @bot.message_handler(commands=['button'])
@@ -31,3 +45,5 @@ def message_reply(message):
 
 # Запуск бота
 bot.polling(none_stop=True, interval=0)
+
+
--- models.py
+++ models.py
@@ -1,32 +1,94 @@
-# import mysql.connector
-# # Установите соединение с базой данных
+from  mysql.connector import *
+
+
+class Base_SQL():
+    def __init__(self) -> None:
+        pass
+    
+    def create_connection():
+        """Подключение к БД"""
+        connection = connect(
+            host="127.0.0.1",
+            user="root",
+            password="",
+            database="bot-sport-bd"
+        )
+        return connection
+        
+    def show(request : str) -> list:
+        """  Принимает sql запрос. Возращает список значений """
+        connection = Base_SQL.create_connection()
+        with connection.cursor() as cursor:
+            cursor.execute(request) # чтение
+            result = cursor.fetchall()
+        connection.close()  # Закрытие соединения
+        return result
+    
+    def write(request : str, data : list):
+        """request - sql запрос. data - данные для записи, формат: [(user.id, user.first_name, str(user.last_name))]"""
+        connection = Base_SQL.create_connection()
+        with connection.cursor() as cursor:
+            cursor.executemany(request, data)
+            connection.commit()
+        connection.close()
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Установите соединение с базой данных
 # def create_connection():
 #     connection = mysql.connector.connect(
-#         host="mysql74.hostland.ru",
-#         user="host1630433_tbotbase",
-#         password="vwwlPmQR",
-#         database="host1630433_tbotbase"
+#         host="127.0.0.1",
+#         user="root",
+#         password="",
+#         database="bot-sport-bd"
 #     )
 #     return connection
 
+
 # base = create_connection()
-# print(base)
+# cursor = base.cursor()
+# query = ("SELECT * FROM User")
+# cursor.execute(query)
+
+# print(cursor)
+# for i in cursor:
+#   print(i)
 
 
 
 
-def verification_user(user : dict) -> bool :
-    """ Авторизация пользователя
-    Принимает юзера. Отдаёт true если найден id, иначе записывает id"""
-    with open("User.txt", "r+") as file:
-        us_id = str(user.id)
-        if not us_id + "\n" in file.readlines():
-            file.write(us_id + "\n")
-            return False
-        return True
+
+# def verification_user(user : dict) -> bool :
+#     """ Авторизация пользователя
+#     Принимает юзера. Отдаёт true если найден id, иначе записывает id"""
+#     with open("User.txt", "r+") as file:
+#         us_id = str(user.id)
+#         if not us_id + "\n" in file.readlines():
+#             file.write(us_id + "\n")
+#             return False
+#         return True
 
 
-   
+
         
     
 
